19327806c5e4d4058995c9ab75bb4ed4c2204311
Christian Byrd <chris10byrd@hotmail.com>
2020-11-22 03:43:28 -0500

    Added product images and a script to populate the database tables.

---
+++ src/main/webapp/populate-storeDB.sql
@@ -0,0 +1,128 @@
+USE storeDB1;
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ( 'Men''s Long Sleeve', '../resources/mens/mens-black-long-sleeve-shirt.jpg',
+         1, 'M', 'shirt', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Ripped Skinny Jeans', '../resources/mens/mens-black-ripped-skinny-jeans.jpg',
+         1, 'M', 'jeans', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Skinny Jeans', '../resources/mens/mens-black-skinny-jeans.jpg',
+         1, 'M', 'jeans', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s T-Shirt', '../resources/mens/mens-black-tshirt.jpg',
+         1, 'M', 'shirt', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Polo', '../resources/mens/mens-brown-polo.jpg',
+         1, 'M', 'shirt', 'brown');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Denim Jeans', '../resources/mens/mens-dark-denim-slim-jeans.jpg',
+         1, 'M', 'jeans', 'navy');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Ripped Skinny Jeans', '../resources/mens/mens-gray-ripped-skinny-jeans.jpg',
+         1, 'M', 'jeans', 'gray');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Skinny Jeans', '../resources/mens/mens-gray-skinny-jeans.jpg',
+         1, 'M', 'jeans', 'gray');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s T-Shirt', '../resources/mens/mens-olive-tshirt.jpg',
+         1, 'M', 'shirt', 'olive');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Polo', '../resources/mens/mens-purple-polo.jpg',
+         1, 'M', 'shirt', 'purple');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Flannel', '../resources/mens/mens-red-flannel.jpg',
+         1, 'M', 'shirt', 'red');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Ripped Skinny Jeans', '../resources/mens/mens-ripped-denim-skinny-jeans.jpg',
+         1, 'M', 'jeans', 'denim');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Ripped Skinny Jeans', '../resources/mens/mens-ripped-light-skinny-jeans.jpg',
+         1, 'M', 'jeans', 'light blue');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s Floral Shirt', '../resources/mens/mens-white-floral-shirt.jpg',
+         1, 'M', 'shirt', 'white');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Men''s T-Shirt', '../resources/mens/mens-white-tshirt.jpg',
+         1, 'M', 'shirt', 'white');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Blouse', '../resources/womens/womens-black-blouse.jpg',
+         1, 'M', 'blouse', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Dress', '../resources/womens/womens-black-dress.jpg',
+         1, 'M', 'dress', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Jeans', '../resources/womens/womens-black-jeans.jpg',
+         1, 'M', 'jeans', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Skirt', '../resources/womens/womens-black-skirt.jpg',
+         1, 'M', 'skirt', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Shirt', '../resources/womens/womens-blue-tshirt.jpg',
+         1, 'M', 'shirt', 'blue');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Skirt', '../resources/womens/womens-brown-skirt.jpg',
+         1, 'M', 'skirt', 'brown');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Graphic T-Shirt', '../resources/womens/womens-graphic-tshirt.jpg',
+         1, 'M', 'shirt', 'white');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Flannel', '../resources/womens/womens-olive-flannel.jpg',
+         1, 'M', 'shirt', 'olive');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Ripped Jeans', '../resources/womens/womens-ripped-black-jeans.jpg',
+         1, 'M', 'jeans', 'black');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Jeans', '../resources/womens/womens-skinny-denim-jeans.jpg',
+         1, 'M', 'jeans', 'denim');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Jeans', '../resources/womens/womens-skinny-denim-jeans.jpg',
+         1, 'M', 'jeans', 'denim');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Jeans', '../resources/womens/womens-slim-blue-jeans.jpg',
+         1, 'M', 'jeans', 'denim');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Skirt', '../resources/womens/womens-styled-skirt.jpg',
+         1, 'M', 'jeans', 'white');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Skirt', '../resources/womens/womens-tan-skirt.jpg',
+         1, 'M', 'jeans', 'tan');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s T-Shirt', '../resources/womens/womens-white-tshirt.jpg',
+         1, 'M', 'shirt', 'white');
+
+INSERT INTO products (name, imgUrl, quantity, size, category, color)
+VALUES ('Women''s Turtle Neck', '../resources/womens/womens-white-turtle-neck.jpg',
+         1, 'M', 'shirt', 'white');
+
+INSERT INTO users (userID, email, firstName, lastName, password, admin)
+VALUES (0, 'admin@storename.com', 'admin', 'admin', 'admin', 1);
